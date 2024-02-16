0c4a1816a2ada83aa7194b6e94866c8f78a54149
andres-rivera-05 <cristianrivera161141@gmail.com>
2024-02-15 19:09:27 -0600

    modificando formulario

---
+++ index.js
@@ -0,0 +1,10 @@
+function calcularDescuento(){
+
+    let producto1 =parseFloat(document.getElementById("Producto1").value);
+    let producto2 =parseFloat(document.getElementById("Producto2").value);
+    let producto3 =parseFloat(document.getElementById("Producto3").value);
+    let producto4 =parseFloat(document.getElementById("Producto4").value);
+    let producto5 =parseFloat(document.getElementById("Producto5").value);
+
+
+}
